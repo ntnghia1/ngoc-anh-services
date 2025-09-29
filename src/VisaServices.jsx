d0815742528324c291a7ccff7535f1212e185fbd
ntnghia1 <ntnghia00@yahoo.com>
2025-09-29 13:59:11 -0600

    Update VisaServices.jsx

--- src/VisaServices.jsx
+++ src/VisaServices.jsx
@@ -85,8 +85,6 @@ try {
     await saveSubmission('visa', rec)
   } catch (_) {}
 
-}
-
   return (
     <div className="max-w-3xl mx-auto p-6 space-y-6">
       <div className="border rounded-xl p-4">
